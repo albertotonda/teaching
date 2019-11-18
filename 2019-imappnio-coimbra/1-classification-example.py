123bcbb1c45daf75b9179f68395e916bf57bcbfe
albertotonda <alberto.tonda@gmail.com>
2019-11-18 15:37:32 +0100

    Titanic dataset is now complete. Classification script is almost complete.

--- 2019-imappnio-coimbra/1-classification-example.py
+++ 2019-imappnio-coimbra/1-classification-example.py
@@ -27,11 +27,15 @@ predictive_features = ['Age', 'Pclass', 'IsAlone', 'Deck', 'Embarked_Q', 'Embark
 X = df[predictive_features].values
 print("Predictive features:", X)
 
+# we split the data into training and test (we will use test data later)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
 # and now, let's MACHINE LEARN THE S**T OUT OF THIS DATASET! uncomment the two lines corresponding to your favorite classification algorithm (or a random one) and LET'S GO 
 
 # Logistic Regression, old but reliable
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegression()
+#from sklearn.linear_model import LogisticRegression
+#model = LogisticRegression()
 
 # Decision Tree, highly interpretable but probably kinda sucky
 #from sklearn.tree import DecisionTreeClassifier
@@ -49,11 +53,30 @@ model = LogisticRegression()
 #from sklearn.ensemble import AdaBoostClassifier
 #model = AdaBoostClassifier(n_estimators=10)
 
+# Bagging is more or less as slow and effective as AdaBoost
+from sklearn.ensemble import BaggingClassifier
+model = BaggingClassifier(n_estimators=10)
+
 # 'fit' trains the model based on the data
-model.fit(X, y)
+model.fit(X_train, y_train)
 # 'predict' performs predictions
-y_predicted = model.predict(X)
+y_train_predicted = model.predict(X_train)
 # and finally, we can compute the classification accuracy, in (0.0, 1.0)
 from sklearn.metrics import accuracy_score
-score = accuracy_score(y, y_predicted)
-print("The classification accuracy score of classifier %s is %.4f" % (model.__class__.__name__, score))
+score_train = accuracy_score(y_train, y_train_predicted)
+print("The classification accuracy score of classifier %s on the training data is %.4f" % (model.__class__.__name__, score_train))
+
+# now, let's see what happens with the classification accuracy on test (unseen data)
+y_test_predicted = model.predict(X_test)
+score_test = accuracy_score(y_test, y_test_predicted)
+print("The classification accuracy score of classifier %s on the test data is %.4f" % (model.__class__.__name__, score_test))
+
+# to obtain a more reliable performance, a classifier can be run multiple times, using random training/test splits, and we can then take the average
+n_folds = 10
+print("Now performing a cross-validation with %d folds (might take a while)..." % n_folds)
+from sklearn.model_selection import cross_validate
+cross_validation_results = cross_validate(model, X, y, scoring='accuracy', cv=n_folds)
+
+# numpy is a module with utility functions for quickly computing functions
+import numpy as np
+print("Average classification accuracy score (on test) for model %s is %.4f (+/- %.4f)" % (model.__class__.__name__, np.mean(cross_validation_results["test_score"]), np.std(cross_validation_results["test_score"])))
